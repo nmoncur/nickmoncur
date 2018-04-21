df84719fe642150e1d3291c479e8d7bbd9ea1674
Nick Moncur <nmoncur10@gmail.com>
2018-04-20 22:30:08 -0600

    Add some yummy Google Analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  @if( Request::root() === 'http://nickmoncur.com' )
+  <!-- Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-64697120-3"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-64697120-3');
+  </script>
+  <!-- end Google Analytics -->
+  @endif
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
