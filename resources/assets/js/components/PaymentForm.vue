9a622b82cd0208dfce315daf946e6a8dee0fc9e0
Nick Moncur <nmoncur10@gmail.com>
2018-05-23 10:33:59 -0600

    Add Pay view and PaymentForm component

---
+++ resources/assets/js/components/PaymentForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form action="" @submit.prevent="submitForm()">
+      <div class="row">
+
+        <div class="form-group col-sm-6">
+          <label for="name" class="sr-only">Your name</label>
+          <input id="name" type="text" placeholder="Your Name" class="form-control">
+        </div>
+
+        <div class="form-group col-sm-6">
+          <label for="email" class="sr-only">Your email address</label>
+          <input id="email" type="email" placeholder="Your Email" class="form-control" required>
+        </div>
+
+        <div class="form-group">
+          <div class="col-3">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+
+          <div v-if="message" class="col-9"><span class="text-alert">{{ message }}</span></div>
+
+        </div>
+
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+  export default {
+    methods: {
+      submitForm() {
+        console.log('submitted')
+      }
+    }
+  }
+</script>
---
+++ resources/assets/js/views/Pay.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+
+    <div class="row">
+      <div class="col-12">
+        <h3>Thank you for choosing me</h3>
+      </div>
+    </div>
+
+    <div class="row text-center">
+      <div class="col-sm-8 col-offset-2">
+        <payment-form></payment-form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import PaymentForm from '../components/PaymentForm'
+
+  export default {
+    components: {
+      PaymentForm
+    }
+  }
+</script>
